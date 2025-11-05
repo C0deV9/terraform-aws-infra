56ee1fa810d5000f687046d2ac169394b01e6839
Sagar170899 <chavan.g.sagar@gmail.com>
2025-11-05 05:53:43 +0000

    Add Terraform apply and EC2 auto-secret update workflows

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
 output "vpc_id" { value = module.vpc.vpc_id }
 output "alb_dns" { value = module.alb.alb_dns_name }
 output "rds_endpoint" { value = module.rds.db_endpoint }
-output "ec2_host" { value = aws_instance.app.public_ip }
+output "ec2_host" { value = module.ec2.instance_public_ip }
