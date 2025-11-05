335d5612c7daa3c5df11cc46038f646300bc083b
Sagar170899 <chavan.g.sagar@gmail.com>
2025-11-05 09:22:03 +0000

    Updated CI/CD

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -1,14 +1,18 @@
 resource "aws_instance" "app" {
-  ami           = "ami-0ecb62995f68bb549"
+  ami           = "ami-0bdd88bd06d16ba03"
   instance_type = "t2.micro"
   subnet_id     = var.subnet_id
   vpc_security_group_ids = [var.security_group_id]
   key_name = var.key_name
   user_data = <<-EOF
     #!/bin/bash
-    sudo apt update -y
-    sudo apt install -y docker.io
+    sudo yum install -y docker
+    sudo systemctl enable docker
     sudo systemctl start docker
+    sudo usermod -aG docker ec2-user
+    sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" \
+      -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
   EOF
 }
 
