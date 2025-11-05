adda4f723734699d00b2f9f423569e9cbddf1874
Sagar170899 <chavan.g.sagar@gmail.com>
2025-11-05 05:29:34 +0000

    Updated files

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,4 @@
 output "vpc_id" { value = module.vpc.vpc_id }
 output "alb_dns" { value = module.alb.alb_dns_name }
 output "rds_endpoint" { value = module.rds.db_endpoint }
+output "ec2_host" { value = aws_instance.app.public_ip }
