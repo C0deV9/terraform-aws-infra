dc3915bb1b5bcb54924ef3d06c7f02b26a5f6106
Sagar170899 <chavan.g.sagar@gmail.com>
2025-11-05 06:27:57 +0000

    Updated vairables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,4 +2,7 @@ variable "aws_region" { default = "us-east-1" }
 variable "state_bucket_name" {}
 variable "state_lock_table" {}
 variable "project_name" { default = "members-crud-app" }
-variable "key_name" { description = "EC2 key pair name" }
+variable "key_name" { 
+    description = "EC2 key pair name"
+    default = "ec2keypair"
+} 
